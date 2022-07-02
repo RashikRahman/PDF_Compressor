e18ab39bd2fa104631b4e54bc890a20513626540
RashikRahmam <rashikrahman420@gmail.com>
2022-07-02 21:27:49 +0600

    update

--- Compress.py
+++ Compress.py
@@ -1,5 +1,7 @@
-from PDFNetPython3 import PDFDoc, Optimizer, SDFDoc
 
+from PDFNetPython3 import *
+
+PDFNet.Initialize("demo:1656775129318:7a4c316403000000002ded2b9985ddf7cce81ba75d69be5b9fa2a7bccf")
 doc = PDFDoc('./origin.pdf')
 doc.InitSecurityHandler()
 Optimizer.Optimize(doc)
